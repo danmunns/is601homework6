0fa82db9f4563d26d2b53a87b785b7f7c0841142
Dan Munns <dmgithub19@icloud.com>
2025-02-10 18:31:56 -0500

    Fixing pylint errors for calculation file

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -15,11 +15,12 @@ class Calculation:
     Creates a calculation object which can be passed to the appropriate 
     mathematical operation and added to calculations history.
     """
-    def __init__(self, num_1: Decimal, num_2: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+    def __init__(self, num_1: Decimal, num_2: Decimal, operation: \
+                 Callable[[Decimal, Decimal], Decimal]):
         self.num_1 = num_1
         self.num_2 = num_2
         self.operation = operation
-    
+
     # Static method to create a new instance of Calculation
     @staticmethod    
     def create(num_1: Decimal, num_2: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
