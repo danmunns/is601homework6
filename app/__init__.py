1f9e9ad7c8f73e20f52af442db7007f0f51b02c3
Dan Munns <dmgithub19@icloud.com>
2025-02-25 23:25:16 -0500

    Added plugins folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,27 @@
-from app.commands import CommandHandler
-from app.commands.add import AddCommand 
-from app.commands.subtract import SubtractCommand 
-from app.commands.multiply import MultiplyCommand 
-from app.commands.divide import DivideCommand
+import pkgutil
+import importlib
 from calculator import Calculator
+from app.commands import CommandHandler, Command
 
 class App:
     def __init__(self): # Constructor
         self.command_handler = CommandHandler()
         self.calculator = Calculator()
-        self.register_commands()
+        self.load_plugins()
 
-    def register_commands(self):
-        # Register commands here
-        self.command_handler.register_command("add", AddCommand(self.calculator))
-        self.command_handler.register_command("subtract", SubtractCommand(self.calculator))
-        self.command_handler.register_command("multiply", MultiplyCommand(self.calculator))
-        self.command_handler.register_command("divide", DivideCommand(self.calculator))
+    def load_plugins(self):
+        # Dynamically load all plugins in the plugins directory
+        plugins_package = 'app.plugins'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:  # Ensure it's a package
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
+                            self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue  # If item is not a class or unrelated class, just ignore
 
     def start(self):
         print("Type 'exit' to exit.")
