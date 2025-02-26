c6080ee205b29e92d8079995bb1b2becbd639d82
Dan Munns <dmgithub19@icloud.com>
2025-02-25 23:34:08 -0500

    Fixes to plugins

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
+import os
 import pkgutil
 import importlib
 from calculator import Calculator
-from app.commands import CommandHandler, Command
+from app.commands import CommandHandler, CalculatorCommand
 
 class App:
     def __init__(self): # Constructor
@@ -12,16 +13,16 @@ class App:
     def load_plugins(self):
         # Dynamically load all plugins in the plugins directory
         plugins_package = 'app.plugins'
-        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
-            if is_pkg:  # Ensure it's a package
-                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
-                for item_name in dir(plugin_module):
-                    item = getattr(plugin_module, item_name)
-                    try:
-                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
-                            self.command_handler.register_command(plugin_name, item())
-                    except TypeError:
-                        continue  # If item is not a class or unrelated class, just ignore
+        package_dir = os.path.dirname(importlib.import_module(plugins_package).__file__)
+        for _, plugin_name, _ in pkgutil.iter_modules([package_dir]):
+            plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+            for item_name in dir(plugin_module):
+                item = getattr(plugin_module, item_name)
+                try:
+                    if issubclass(item, CalculatorCommand) and item is not CalculatorCommand:
+                        self.command_handler.register_command(plugin_name, item(self.calculator))
+                except TypeError:
+                    continue  # If item is not a class or unrelated class, just ignore
 
     def start(self):
         print("Type 'exit' to exit.")
