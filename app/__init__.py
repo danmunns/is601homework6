0e5b05dddea80b561de39bc68961f86894fc618e
Dan Munns <dmgithub19@icloud.com>
2025-03-04 20:52:34 -0500

    Added basic logging features

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,28 @@
 import os
 import pkgutil
 import importlib
+import sys
+from dotenv import load_dotenv
+import logging
+import logging.config
 from calculator import Calculator
 from app.commands import CommandHandler, CalculatorCommand
 
 class App:
     def __init__(self): # Constructor
+        os.makedirs('logs', exist_ok=True)
+        self.configure_logging()
         self.command_handler = CommandHandler()
         self.calculator = Calculator()
         self.load_plugins()
+    
+    def configure_logging(self):
+        logging_conf_path = 'logging.conf'
+        if os.path.exists(logging_conf_path):
+            logging.config.fileConfig(logging_conf_path, disable_existing_loggers=False)
+        else:
+            logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info("Logging configured.")
 
     def load_plugins(self):
         # Dynamically load all plugins in the plugins directory
